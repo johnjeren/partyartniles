23fe84fda2770a00b4bc3a7a854b14f6b6e7eea2
JJ3 <jjeren3@gmail.com>
2025-05-29 01:56:27 -0400

    update

--- wp-content/themes/base-theme/functions.php
+++ wp-content/themes/base-theme/functions.php
@@ -12,4 +12,10 @@ if( function_exists('acf_add_options_page') ) {
 
 }
 
+add_action('init', function() {
+    if (class_exists('ActionScheduler') && function_exists('as_unschedule_all_actions')) {
+        ActionScheduler::init();
+    }
+}, 1);
+
 
